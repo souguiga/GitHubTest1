1eb8db54bad27554b86bf21bac6f65a0d57849cf
Guilherme de Souza <guigasouza222@gmail.com>
2019-11-19 10:03:37 -0700

    Gui changed

---
+++ GitHubTest1/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitHubTest1.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            string = "Gui is awesome";
+            return View();
+           
+        }
+    }
+}
